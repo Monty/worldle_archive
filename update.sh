63976508c8b27b22ef1f1de6550a754c472542c8
Monty Williams <monty.williams@gmail.com>
2022-03-02 14:55:51 -0800

    Fix broken update script

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 
 # Build a single page website and prettify the HTML
 npx --yes -- single-page-markdown-website 'w*.md' --open
-prettier-eslint --print-width 95 >docs/index.html
+prettier-eslint --print-width 95 build/index.html >docs/index.html
