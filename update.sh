46332f140c98e8e1e2ec00e7f687a68e7141a138
Monty Williams <monty.williams@gmail.com>
2022-05-21 13:01:15 -0700

    Fix -1.svg problem

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 # Build a single page website and prettify the HTML
-npx --yes -- single-page-markdown-website 'w*.md' --open
-prettier-eslint --print-width 95 build/index.html >docs/index.html
+npx --yes -- single-page-markdown-website 'w*.md'
+prettier-eslint --print-width 95 build/index.html |
+    perl -p -e 's+-1.svg+.svg+g' >docs/index.html
+open docs/index.html
